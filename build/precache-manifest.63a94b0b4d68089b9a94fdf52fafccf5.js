151cf648fa891949cd307b481d560217f4c71830
Arun-Raj-B <sent2arunraj@gmail.com>
2023-01-18 22:46:14 +0530

    build added

---
+++ build/precache-manifest.63a94b0b4d68089b9a94fdf52fafccf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff2ac1e4b1d55c411d1",
+    "url": "/static/css/main.f6e58ea2.chunk.css"
+  },
+  {
+    "revision": "8ff2ac1e4b1d55c411d1",
+    "url": "/static/js/main.8ff2ac1e.chunk.js"
+  },
+  {
+    "revision": "5ca2888d7537dd2a424c",
+    "url": "/static/js/1.5ca2888d.chunk.js"
+  },
+  {
+    "revision": "e163d8e3306243f664fc",
+    "url": "/static/css/2.f20b520c.chunk.css"
+  },
+  {
+    "revision": "e163d8e3306243f664fc",
+    "url": "/static/js/2.e163d8e3.chunk.js"
+  },
+  {
+    "revision": "7f4278f1abe46217ed0f",
+    "url": "/static/js/runtime~main.7f4278f1.js"
+  },
+  {
+    "revision": "af4be9f3f35ef22cbd180606f1048be5",
+    "url": "/static/media/background1.af4be9f3.jpg"
+  },
+  {
+    "revision": "e5c953e87d9aae182296dce6b383ddb0",
+    "url": "/index.html"
+  }
+];
